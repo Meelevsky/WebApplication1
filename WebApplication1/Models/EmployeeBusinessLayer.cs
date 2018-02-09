e96ce33eabdc731e08a15dec536c39331fdbcba7
Maciej Milewski <maciej.milewski@pl.abb.com>
2018-02-09 11:27:04 +0100

    Add project files.

---
+++ WebApplication1/Models/EmployeeBusinessLayer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class EmployeeBusinessLayer
+    {
+        public List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            Employee emp = new Employee();
+            emp.FirstName = "Lukasz";
+            emp.LastName = "Walenciak";
+            emp.Salary = 14000;
+            employees.Add(emp);
+
+
+            emp = new Employee();
+            emp.FirstName = "Pawel";
+            emp.LastName = "Garuski";
+            emp.Salary = 20000;
+            employees.Add(emp);
+
+
+            emp = new Employee();
+            emp.FirstName = "Maciej";
+            emp.LastName = "Milewski";
+            emp.Salary = 6210;
+            employees.Add(emp);
+
+            return employees;
+        }
+    }
+}
